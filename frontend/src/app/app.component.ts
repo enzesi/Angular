01bae7af31ce9e07458b71b3730761fa92773641
enzesi <32287882+enzesi@users.noreply.github.com>
2018-11-10 12:42:47 -0500

    sort

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient}    from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) { }
+
   title = 'frontend';
+  input = '5,3,2,1,4';
+  url = "http://localhost:8080/sort.json?input=1,2,3,4,6&sort_type=1&value_type=2";
+  result = "";
+  sort() {
+    this.http.get(this.url)
+    .subscribe(data => {
+        var info : string = data['result'];
+        this.result = info;
+        
+      },
+      err => console.log(err)
+    );
+  }
+
 }
